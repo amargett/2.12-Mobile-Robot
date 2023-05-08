1524299af9ce0199bfc28f41160665186e114f9b
Blenn24 <bradynlenning@gmail.com>
2023-05-07 22:23:39 -0400

    ugh

--- src/test_code/unimportant_file.py
+++ src/test_code/unimportant_file.py
@@ -72,6 +72,8 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     #go straight
+    if not cone_position:
+        continue
     if(abs(cone_position[0] - midpoint)< midpoint/6):
         left_desired_vel = -3
         right_desired_vel = -3
