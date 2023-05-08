c98577e9f78c1c8e95ef295f1fd6d682b92f9dfa
joshsohn <jcsohn@mit.edu>
2023-05-07 23:11:30 -0400

    avoid cone

--- src/test_code/drive3.py
+++ src/test_code/drive3.py
@@ -1,9 +1,13 @@
 import serial
 import time
 import numpy as np
+import cv2
 
 arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, timeout=.1)
 # arduino = serial.Serial(port='/dev/tty.usbserial-0264FEA5', baudrate=115200, timeout=.1)
+
+orange_lower = np.array([1, 100, 150])
+orange_upper = np.array([15, 200, 255])
 
 STRAIGHT_VEL = 5
 TURN_VEL = STRAIGHT_VEL/2
@@ -13,6 +17,11 @@ ALPHA = 0.15
 EPSILON_HEADING = 1
 EPISLON_DIST = 0.1
 K_HEADING = 0.05
+cap = cv2.VideoCapture(0)
+SCREEN_WIDTH = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+SCREEN_HEIGHT = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+MIDPOINT = SCREEN_WIDTH // 2
+
 
 def april_tag(): 
     # detects whether there is an april tag in view
@@ -55,6 +64,10 @@ def main():
         if [car.x_raw, car.y_raw, car.heading_raw] != [None, None, None] and (time.time() - car.prev_time) > 1e-3: 
             car.setXYH()
             # print(car.x, car.y, car.heading)
+            car.look_for_cone()
+            if car.cone_position: 
+                car.avoid_cone()
+                pass
             if car.state == 0: ## go to AED waypoint
                 car.target_x = 1
                 car.target_y = 1.7
@@ -64,7 +77,7 @@ def main():
                     car.mini_state = 0
                     car.state = 1
             if car.state == 1: # go to AED and pick it up
-                car.target_x = 0
+                car.target_x = -0.1
                 car.target_y = 1.65
                 car.go()
                 if car.mini_state == 2:
@@ -130,6 +143,7 @@ class Car(object):
         self.dropoff_counter = 0
 
         self.mini_state = 0
+        self.cone_position = None
 
     def readArduino(self):
         '''
@@ -218,6 +232,49 @@ class Car(object):
             if abs(self.x - self.target_x) < EPISLON_DIST and abs(self.y - self.target_y) < EPISLON_DIST:
                 self.mini_state = 2
         return self.mini_state
+    
+    def look_for_cone(self): 
+        # Read the frame from the video capture
+        ret, frame = cap.read()
+        if not ret:
+            print("Failed to capture frame from camera")
+            return
+        # Convert the frame to the HSV color space
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        # Create a mask based on the orange color range
+        mask = cv2.inRange(hsv, orange_lower, orange_upper)
+        # Perform morphological operations to remove noise
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
+        # Find contours in the mask
+        contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # Initialize the position of the cone
+        self.cone_position = None
+        # Process the contours
+        if len(contours) > 0:
+            # Find the largest contour
+            largest_contour = max(contours, key=cv2.contourArea)
+            # Calculate the center of the contour
+            M = cv2.moments(largest_contour)
+            if M["m00"] > 0:
+                cx = int(M["m10"] / M["m00"])
+                cy = int(M["m01"] / M["m00"])
+                self.cone_position = (cx, cy)
+                # Draw a circle at the center of the contour
+                cv2.circle(frame, self.cone_position, 5, (0, 255, 0), -1)
+        # Display the frame with the cone position
+        cv2.imshow("Traffic Cone Detection", frame)
+        return
+    
+    def avoid_cone(self):
+        if(abs(self.cone_position[0] - MIDPOINT)< MIDPOINT/6):
+            self.straight()
+        elif(self.cone_position[0] < MIDPOINT):
+            self.leftVel = -STRAIGHT_VEL
+            self.rightVel = -STRAIGHT_VEL/3
+        else:
+            self.leftVel = -STRAIGHT_VEL/3
+            self.rightVel = -STRAIGHT_VEL
 
 if __name__ == "__main__":
     main()
