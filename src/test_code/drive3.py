1d223caf9a4e9ee1eb2ac989359a387f51ce6cd4
zhangyutong0318 <128844398+zhangyutong0318@users.noreply.github.com>
2023-05-09 01:47:52 -0400

    Update drive3.py

--- src/test_code/drive3.py
+++ src/test_code/drive3.py
@@ -327,12 +327,15 @@ class Car(object):
         self.frame = cv2.resize(self.frame, (640,480))
         gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
         tags = detector.detect(gray, estimate_tag_pose=False, camera_params=self.intrisic, tag_size=self.tagsize)
+        
         if tags:
-            print("APRIL TAG")
+            print("tag_0 center :",tags[0].center[0])
             tag = tags[0]
             if tag.center[0] < 320 - 10:
+                #print("turn left")
                 return 1   #turn left
             elif tag.center[0] > 320 + 10:
+                #print("turn right")
                 return 2   #turn right
             else:
                 return 3   # go straight
