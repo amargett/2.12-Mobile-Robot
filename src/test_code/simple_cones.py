1a3bd8280004e8add97f17864de58f6923f2cce8
zhangyutong0318 <128844398+zhangyutong0318@users.noreply.github.com>
2023-05-09 03:40:23 -0400

    Update simple_cones.py

--- src/test_code/simple_cones.py
+++ src/test_code/simple_cones.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+import time
+
 
 def find_orange_cone():
     # Open the webcam
@@ -10,9 +12,17 @@ def find_orange_cone():
     screen_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     midpoint = screen_width // 2
 
+    # Initialize variables for frame voting
+    #frame_count = 0
+    #cone_detected_count = 0
+    vote_array = []
+    
+
     while True:
         # Read the frame from the webcam
         ret, frame = cap.read()
+        #frame_count += 1
+        #print(frame_count)
         if not ret:
             print("Failed to capture frame from camera")
             break
@@ -30,14 +40,15 @@ def find_orange_cone():
         # Perform morphological operations to remove noise
         mask = cv2.erode(mask, None, iterations=2)
         mask = cv2.dilate(mask, None, iterations=2)
-        cv2.imshow("Orange Cone Detection", mask)
+
         # Find contours in the mask
         contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        cv2.imshow("Orange Cone Detection", mask)
         # Initialize the position of the largest contour
         largest_contour_position = None
 
+        cone_detected = 0
+        state = 0
         # Process the contours
         if len(contours) > 0:
             # Find the largest contour
@@ -55,20 +66,50 @@ def find_orange_cone():
                     cy = int(M["m01"] / M["m00"])
                     cv2.circle(frame, (cx, cy), 10, (0, 255, 0), -1)
 
+                    # Increment the cone detected count
+                    cone_detected = 1
+                    #print("cone_detected = ", cone_detected_count)
+
                     # Print the position of the cone
-                    #print("Cone position: ({}, {})".format(cx, cy))
-                    """
                     if cx < midpoint:
                         print("cone on left")
+                        state = 1
+                        
                     else:
                         print("cone on right")
-                    """
+                        state = 2
             else:
                 print("no cone")
-        # Display the frame with the largest contour position
-        #cv2.imshow("Orange Cone Detection", frame)
+            #     # Add a 0 to the vote array if no cone is detected in this frame
+            #     vote_array.append(0)
+        # else:
+        #     print("no cone")
+        #     # Add a 0 to the vote array if no cone is detected in this frame
+        #     vote_array.append(0)
+            
+        
+        vote_array.append(cone_detected)
+        
 
-        # Exit the loop if the 'q' key is pressed
+        
+
+        # If the vote array has more than 9 elements, remove the oldest one
+        if len(vote_array) > 15:
+            vote_array.pop(0)
+            
+
+        # Determine the state of the current frame based on the vote array
+        if sum(vote_array) > len(vote_array) / 2 and state == 1:
+            print ("cone detected on left_determined")
+        elif sum(vote_array) > len(vote_array) / 2 and state == 2:
+            print ("cone detected on right_determined")
+        elif sum(vote_array) > len(vote_array) / 2 and state == 0:
+            print ("maybe false negative")
+            
+        else:
+            print ("no cone determined")
+            
+        #time.sleep(1)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
