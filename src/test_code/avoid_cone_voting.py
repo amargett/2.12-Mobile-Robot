863cfedfceaf59629c6c65a161ed9bf15587f2a6
zhangyutong0318 <128844398+zhangyutong0318@users.noreply.github.com>
2023-05-09 16:03:50 -0400

    Update avoid_cone_voting.py

--- src/test_code/avoid_cone_voting.py
+++ src/test_code/avoid_cone_voting.py
@@ -67,7 +67,7 @@ while True:
         print("largest_contour", largest_contour_area)
 
         # Check if the largest contour has a large enough magnitude (area)
-        if largest_contour_area > 5000:
+        if largest_contour_area > 2500:
             # Calculate the center of the contour
             M = cv2.moments(largest_contour)
             if M["m00"] > 0:
