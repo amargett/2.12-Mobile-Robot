3a881532ac89a3583067a9985abd6e8f2bc256d4
Blenn24 <bradynlenning@gmail.com>
2023-05-09 21:39:15 -0400

    A

--- src/test_code/jetson_socket.py
+++ src/test_code/jetson_socket.py
@@ -1,30 +1,43 @@
 import socket
+import json
 
 # IP address and port of the Linux computer
 host = '10.29.100.148'
 port = 12345
 
-# Float values to send
-float_values = [1.23, 4.56, 7.89]
-
 # Create a socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+def send_data(data):
+    # Convert data to JSON string
+    json_data = json.dumps(data)
+
+    # Send the JSON string
+    sock.sendall(json_data.encode())
+
+    # Receive the JSON string
+    received_data = sock.recv(1024).decode()
+
+    # Parse the JSON string to get the received data
+    received_data = json.loads(received_data)
+
+    # Return the received data
+    return received_data
+
 # Connect to the Linux computer
 sock.connect((host, port))
+print('Connected to', host)
 
-# Send the float values
-for value in float_values:
-    sock.sendall(str(value).encode())
+# Continuously send and receive data
+while True:
+    # Data to send
+    data_to_send = [1.23, 4.56, 7.89]
 
-# Receive the float values from the Linux computer
-received_values = []
-for _ in range(3):
-    data = sock.recv(1024).decode()
-    received_values.append(float(data))
+    # Send and receive data
+    received_data = send_data(data_to_send)
 
-# Print the received float values
-print('Received values:', received_values)
+    # Print the received data
+    print('Received data:', received_data)
 
 # Close the socket
 sock.close()
