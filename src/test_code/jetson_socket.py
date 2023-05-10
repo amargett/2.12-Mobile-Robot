e3c56d532ae6d79ca3c7425fcf729e4374156ed3
Blenn24 <bradynlenning@gmail.com>
2023-05-09 21:11:26 -0400

    A

---
+++ src/test_code/jetson_socket.py
@@ -0,0 +1,30 @@
+import socket
+
+# IP address and port of the Linux computer
+host = '192.168.0.100'
+port = 12345
+
+# Float values to send
+float_values = [1.23, 4.56, 7.89]
+
+# Create a socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to the Linux computer
+sock.connect((host, port))
+
+# Send the float values
+for value in float_values:
+    sock.sendall(str(value).encode())
+
+# Receive the float values from the Linux computer
+received_values = []
+for _ in range(3):
+    data = sock.recv(1024).decode()
+    received_values.append(float(data))
+
+# Print the received float values
+print('Received values:', received_values)
+
+# Close the socket
+sock.close()
---
+++ src/test_code/linux_socket.py
@@ -0,0 +1,38 @@
+import socket
+
+# IP address and port to listen on
+host = '192.168.0.100'
+port = 12345
+
+# Create a socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the address
+sock.bind((host, port))
+
+# Listen for incoming connections
+sock.listen(1)
+
+# Accept a connection
+conn, addr = sock.accept()
+print('Connected by', addr)
+
+# Receive the float values from the Jetson device
+received_values = []
+for _ in range(3):
+    data = conn.recv(1024).decode()
+    received_values.append(float(data))
+
+# Print the received float values
+print('Received values:', received_values)
+
+# Float values to send back
+float_values = [9.87, 6.54, 3.21]
+
+# Send the float values back to the Jetson device
+for value in float_values:
+    conn.sendall(str(value).encode())
+
+# Close the connection and socket
+conn.close()
+sock.close()
